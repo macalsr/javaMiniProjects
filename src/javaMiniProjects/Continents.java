c38210e5cc0dfe8ced69d19e488ef4c888756fc6
macalsr <mmariasribeiro@gmail.com>
2021-12-14 20:46:56 -0300

    Mini projects for study.

---
+++ src/javaMiniProjects/Continents.java
@@ -0,0 +1,20 @@
+package javaMiniProjects;
+//A program that will print out a continent and the largest city in that continent, based on the value of an integer.
+
+public class Continents {
+    int continent = 4;
+
+    public void continents() {
+
+        switch(continent){
+            case 1:
+            System.out.println("North America: Mexico City, Mexico");
+
+        }
+    }
+    public static void main(String[] args) {
+
+
+
+    }
+}
